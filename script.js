587b4107a6b29cf5ac999417fb30ad82578062b1
NICKY CHIBILIKA <53586666+nickych@users.noreply.github.com>
2023-11-28 13:32:39 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const userBbox = () => {
+    let boxS = document.getElementById("user-box")  //get the infromation from an HTML element
+    user-box.addEventListener("submit", (event) => {
+            event.preventDefault(); //prevent the userBbox from submiting 
+        } 
+
+};
+
+const name = document.getElementById("nameInput");
+
+console.log("submitted name: ", name);
+
+
+const userBbox = () => {
+    let boxS = document.getElementById("userr_body"); //gets the information from an HTML element
+    user-box.addEventListener("submit", (event) => {
+            event.preventDefault(); //prevent the userBbox from submiting 
+        } 
+
+};
+
+const nameInput = document.getElementById("anyNameInput");
+
+console.log("submitted name: ", nameInput);
